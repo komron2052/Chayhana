b37e9dfe7e3e80309109cae206fa890c6dcc896f
komron2052@gmail.com <komron2052@gmail.com>
2023-04-16 14:04:35 +0500

    Added IRepository and Repositoy and implemented

---
+++ src/Chayhana.Data/IRepositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Chayhana.Domain.Commons;
+using System.Linq.Expressions;
+
+namespace Chayhana.Data.IRepositories;
+
+public interface IRepository<TEntity> where TEntity : Auditable
+{
+    ValueTask<TEntity> InsertAsync(TEntity entity);
+    TEntity Update(TEntity entity);
+    ValueTask<bool> DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+    ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> predicate, string[] includes = null);
+    IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> predicate = null, string[] includes = null, bool isTracking = true);
+    ValueTask SaveAsync();
+}
---
+++ src/Chayhana.Data/Repositories/Repository.cs
@@ -0,0 +1,63 @@
+﻿using Chayhana.Data.DbContexts;
+using Chayhana.Data.IRepositories;
+using Chayhana.Domain.Commons;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Chayhana.Data.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
+{
+    private readonly ChayhanaDbContext dbContext;
+    private readonly DbSet<TEntity> dbSet;
+    public Repository(ChayhanaDbContext dbContext, DbSet<TEntity> dbSet)
+    {
+        this.dbContext = dbContext;
+        this.dbSet = dbContext.Set<TEntity>();
+    }
+
+    public async ValueTask<bool> DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        var entity = await this.dbSet.FirstOrDefaultAsync(predicate);
+        if (entity == null)
+            return false;
+
+        this.dbSet.Remove(entity);
+        
+        return true;
+    }
+
+    public async ValueTask<TEntity> InsertAsync(TEntity entity)
+    {
+        var entry = await this.dbSet.AddAsync(entity);
+
+        return entry.Entity;
+    }
+
+    public async ValueTask SaveAsync() =>
+        this.dbContext.SaveChanges();
+
+    public IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> predicate = null, string[] includes = null, bool isTracking = true)
+    {
+        var entities = predicate is null ? dbSet : dbSet.Where(predicate);
+
+        if(includes is not null)
+            foreach(var include in includes)
+                entities= entities.Include(include);    
+
+        if (!isTracking)
+            entities=entities.AsNoTracking();
+
+        return entities;
+    }
+
+    public async ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> predicate, string[] includes = null) =>
+        await SelectAll(predicate, includes).FirstOrDefaultAsync();
+
+    public TEntity Update(TEntity entity)
+    {
+        var entry = this.dbSet.Update(entity);
+
+        return entry.Entity;    
+    }
+}
