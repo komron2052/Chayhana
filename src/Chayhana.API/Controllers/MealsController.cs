e93d19b35c6e29d17b5021813c144f4cd2a49920
komron2052@gmail.com <komron2052@gmail.com>
2023-04-17 00:57:45 +0500

    implemented controllers

---
+++ src/Chayhana.API/Controllers/MealsController.cs
@@ -0,0 +1,39 @@
+﻿using Chayhana.Domain.Configurations;
+using Chayhana.Service.DTOs;
+using Chayhana.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chayhana.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        private readonly IMealService mealMervice;
+
+        public MealsController(IMealService mealMervice)
+        {
+            this.mealMervice = mealMervice;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(MealForCreationDto dto) =>
+            Ok(await mealMervice.AddAsync(dto));
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync([FromRoute]int id) =>
+            Ok(await mealMervice.RetrieveByIdAsync(id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params, string searchString) =>
+            Ok(await mealMervice.RetrieveAllAsync(@params, searchString));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id) =>
+            Ok(await mealMervice.RemoveAsync(id));
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(int id, MealForCreationDto dto) =>
+            Ok(await mealMervice.ModifyAsync(id, dto));
+    }
+}
---
+++ src/Chayhana.API/Controllers/OrdersConroller.cs
@@ -0,0 +1,38 @@
+﻿using Chayhana.Domain.Configurations;
+using Chayhana.Service.DTOs;
+using Chayhana.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chayhana.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersConroller : ControllerBase
+{
+    private readonly IOrderService orderService;
+
+    public OrdersConroller(IOrderService orderService)
+    {
+        this.orderService = orderService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(OrderForCreationDto dto) =>
+        Ok(await orderService.AddAsync(dto));
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync([FromRoute] int id) =>
+        Ok(await orderService.RetrieveByIdAsync(id));
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllAsync([FromQuery] PaginationParams @params, string searchString) =>
+        Ok(await orderService.RetrieveAllAsync(@params, searchString));
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] int id) =>
+    Ok(await orderService.RemoveAsync(id));
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync([FromRoute] int id, OrderForCreationDto dto) =>
+    Ok(await orderService.ModifyAsync(id, dto));
+}
